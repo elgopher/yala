7caeb31e3bab431562ba4aeb25dfef9e07c3f94c
Jacek Olszak <jacekolszak@gmail.com>
2022-01-20 18:26:10 +0100

    add better docs for glog example

--- adapter/glogadapter/_example/main.go
+++ adapter/glogadapter/_example/main.go
@@ -14,10 +14,10 @@ var ErrSome = errors.New("ErrSome")
 func main() {
 	ctx := context.Background()
 
-	flag.Parse()                             // glog does not work without parsing the flags first
+	flag.Parse()                             // glog will pick command line options like -stderrthreshold=[INFO|WARNING|ERROR]
 	logger.SetAdapter(glogadapter.Adapter{}) // set glog adapter globally
 
-	logger.Debug(ctx, "Hello glog ")
+	logger.Debug(ctx, "Hello glog ") // Debug will be logged as Info
 	logger.With(ctx, "field_name", "field_value").Info("Some info")
 	logger.With(ctx, "parameter", "some").Warn("Deprecated configuration parameter. It will be removed.")
 	logger.WithError(ctx, ErrSome).Error("Error occurred")
