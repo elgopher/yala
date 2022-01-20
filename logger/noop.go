ccea4faabf634efcb783457e47a3846f42e3c255
Jacek Olszak <jacekolszak@gmail.com>
2022-01-21 00:35:48 +0100

    Add warning that no global logger is set
    Print warning message that global logger was not set. This is a hint for the library user, that the lib is trying to log something, but logging is disabled.

--- logger/noop.go
+++ logger/noop.go
@@ -2,8 +2,22 @@ package logger
 
 import (
 	"context"
+	"fmt"
+	"sync"
 )
 
 type noopLogger struct{}
 
 func (n noopLogger) Log(context.Context, Entry) {}
+
+var once sync.Once
+
+type initialNoopLogger struct{}
+
+func (g *initialNoopLogger) Log(_ context.Context, entry Entry) {
+	if entry.Level == WarnLevel || entry.Level == ErrorLevel {
+		once.Do(func() {
+			fmt.Printf("github.com/jacekolszak/yala/logger cannot log message with level %s. Please set the global logging adapter. For example: logger.SetAdapter(printer.StdoutAdapter()) to log all messages to stdout.\n", entry.Level) // nolint
+		})
+	}
+}
