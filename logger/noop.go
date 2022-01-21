c9e6c82aad439840797ce3230f88ded38838d16a
Jacek Olszak <jacekolszak@gmail.com>
2022-01-22 00:24:36 +0100

    [refactor] rename initialNoopLogger to initialGlobalNoopLogger

--- logger/noop.go
+++ logger/noop.go
@@ -12,9 +12,9 @@ func (n noopLogger) Log(context.Context, Entry) {}
 
 var once sync.Once
 
-type initialNoopLogger struct{}
+type initialGlobalNoopLogger struct{}
 
-func (g *initialNoopLogger) Log(_ context.Context, entry Entry) {
+func (g *initialGlobalNoopLogger) Log(_ context.Context, entry Entry) {
 	if entry.Level == WarnLevel || entry.Level == ErrorLevel {
 		once.Do(func() {
 			fmt.Printf("github.com/jacekolszak/yala/logger cannot log message with level %s. Please set the global logging adapter. For example: logger.SetAdapter(printer.StdoutAdapter()) to log all messages to stdout.\n", entry.Level) // nolint
