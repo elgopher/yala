d403bb9aa866f292bae699212da648532c3d762d
Jacek Olszak <jacekolszak@gmail.com>
2022-01-21 19:24:59 +0100

    [logfmt] Create reusable package to write logfmt messages
    Logfmt Fields and error formatting can be reused in other adapters. This commit extract this functionality from printer package.

---
+++ adapter/logfmt/logfmt.go
@@ -0,0 +1,66 @@
+// Package logfmt provides functions encoding logger.Field using logfmt format, for example: "field=value".
+package logfmt
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/jacekolszak/yala/logger"
+)
+
+func WriteField(builder *strings.Builder, f logger.Field) {
+	builder.WriteString(f.Key)
+	builder.WriteByte('=')
+	writeValue(builder, f.Value)
+}
+
+func writeValue(builder *strings.Builder, value interface{}) {
+	if value == nil {
+		builder.WriteString("nil")
+
+		return
+	}
+
+	if value == "nil" {
+		builder.WriteString(`"nil"`)
+
+		return
+	}
+
+	valueStr := fmt.Sprintf("%s", value)
+
+	if strings.ContainsRune(valueStr, '\\') {
+		valueStr = strings.ReplaceAll(valueStr, `\`, `\\`)
+	}
+
+	if strings.ContainsRune(valueStr, '"') {
+		valueStr = strings.ReplaceAll(valueStr, `"`, `\"`)
+	}
+
+	requiresQuoting := false
+
+	if strings.ContainsRune(valueStr, ' ') || strings.ContainsRune(valueStr, '=') {
+		requiresQuoting = true
+	}
+
+	if requiresQuoting {
+		builder.WriteByte('"')
+	}
+
+	builder.WriteString(valueStr)
+
+	if requiresQuoting {
+		builder.WriteByte('"')
+	}
+}
+
+func WriteFields(builder *strings.Builder, fields []logger.Field) {
+	for i, f := range fields {
+		WriteField(builder, f)
+
+		notLast := i < len(fields)-1
+		if notLast {
+			builder.WriteByte(' ')
+		}
+	}
+}
