c49c0ac03bb7e3563cf5edafe2562fa3cb7d2631
Jacek Olszak <jacekolszak@gmail.com>
2022-01-22 12:28:13 +0100

    [zap] Add zapadapter unit test

---
+++ adapter/zapadapter/zapadapter_test.go
@@ -0,0 +1,26 @@
+package zapadapter_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/jacekolszak/yala/adapter/zapadapter"
+	"github.com/jacekolszak/yala/logger"
+	"github.com/stretchr/testify/assert"
+)
+
+const message = "message"
+
+func TestAdapter_Log(t *testing.T) {
+	ctx := context.Background()
+
+	t.Run("should not panic when logger is nil", func(t *testing.T) {
+		adapter := zapadapter.Adapter{Logger: nil}
+		assert.NotPanics(t, func() {
+			adapter.Log(ctx, logger.Entry{
+				Level:   logger.InfoLevel,
+				Message: message,
+			})
+		})
+	})
+}
