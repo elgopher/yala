bc4b777a9826243cc1751d5e94b8538ed3113c5a
Jacek Olszak <jacekolszak@gmail.com>
2022-01-22 21:33:59 +0100

    [docs] Document contextadapter

--- adapter/contextadapter/contextadapter.go
+++ adapter/contextadapter/contextadapter.go
@@ -1,3 +1,5 @@
+// Package contextadapter provides logger.Adapter implementation which lookups logger in the context.Context and wrap
+// it inside adapter. It is useful when your app is using context.Context to store logger instance.
 package contextadapter
 
 import (
