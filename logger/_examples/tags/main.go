e217ffb78a56abdef5f171578436b0828795a530
Jacek Olszak <jacekolszak@gmail.com>
2022-01-25 20:01:30 +0100

    [merge] Remove the merge package
    This package is not very useful. It does not do anything special. It was created solely to present how to write adapters merging context values with logger entries. But it turns out that another package complicates things for newcomers. It will be better to remove it, but leave the example presenting how to merge context value with entry.

--- logger/_examples/tags/main.go
+++ logger/_examples/tags/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 
-	"github.com/elgopher/yala/adapter/merge"
 	"github.com/elgopher/yala/adapter/printer"
 	"github.com/elgopher/yala/logger"
 )
@@ -15,8 +14,8 @@ func main() {
 	adapter := printer.StdoutAdapter()
 
 	// creates an adapter which adds field from context to each logged message.
-	mergeAdapter := merge.Adapter{Adapter: adapter, MergeFunc: addFieldFromContext}
-	l := logger.Local(mergeAdapter)
+	addFieldAdapter := AddFieldFromContextAdapter{Adapter: adapter}
+	l := logger.Local(addFieldAdapter)
 
 	ctx := context.Background()
 	// add tag to context
@@ -26,12 +25,19 @@ func main() {
 	l.With(ctx, "k", "v").Info("tagged message") // INFO tagged message k=v tag=value
 }
 
-func addFieldFromContext(ctx context.Context, entry logger.Entry) logger.Entry {
-	tagValue := ctx.Value(tag)
+// AddFieldFromContextAdapter is a middleware (decorator) which adds
+// a new field to logger.Entry from the tag stored in the context.
+type AddFieldFromContextAdapter struct {
+	Adapter logger.Adapter
+}
 
-	return entry.With(
+func (a AddFieldFromContextAdapter) Log(ctx context.Context, entry logger.Entry) {
+	// entry.With creates an entry and adds a new field to it
+	newEntry := entry.With(
 		logger.Field{
 			Key:   tag,
-			Value: tagValue,
-		})
+			Value: ctx.Value(tag),
+		},
+	)
+	a.Adapter.Log(ctx, newEntry)
 }
