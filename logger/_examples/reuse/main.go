775c9125bc2caa2b5fed5fad329be81f10813c8d
Jacek Olszak <jacekolszak@gmail.com>
2022-01-26 21:15:48 +0100

    [examples] add example about logger reuse
    This example shows how to reuse loggers

---
+++ logger/_examples/reuse/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+
+	"github.com/elgopher/yala/adapter/printer"
+	"github.com/elgopher/yala/logger"
+)
+
+// This example shows how to reuse loggers
+func main() {
+	ctx := context.Background()
+
+	log := logger.Local(printer.StdoutAdapter())
+
+	// requestLogger will log all messages with at least two fields: request_id and user
+	requestLogger := log.With(ctx, "request_id", "123").With("user", "elgopher")
+
+	requestLogger.Debug("request started")
+	requestLogger.With("table", "gophers").Debug("sql update executed")
+	requestLogger.Debug("request finished")
+}
